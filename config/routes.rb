fd97cd25dfb415e7b4bd708f8d7b7b2842213538
Ashwin Khanna <33923739+ashToronto@users.noreply.github.com>
2024-01-25 01:32:33 -0500

    #9 home page, dashboard if user logged in with sidebar, controller added also (#10)

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "home#index"
+  get '/dashboard', to: 'home#dashboard', as: 'dashboard'
 end
