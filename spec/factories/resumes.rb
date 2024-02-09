acc389bac3b7327051f439125fcf0c194ab8601b
ashwin <ashwin.ashwinkhanna@gmail.com>
2024-02-09 15:23:15 -0500

    fix failing validation test i.e factory botfor resumes

--- spec/factories/resumes.rb
+++ spec/factories/resumes.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :resume do
     user { nil }
+    first_name { 'John' }
+    last_name { 'Doe' }
+    country { 'Canada' }
+    state { 'Ontario' }
+    phone_number { '4165693279' }
+    email { 'JohnDoe@gmail.com' }
+    job_title { 'Software Engineer' }
   end
 end
