2a42986d93ccc5c9fea9614ea08b592ff60c9330
Ashwin Khanna <ashwin.ashwinkhanna@gmail.com>
2024-04-15 23:02:04 -0400

    undo useless code i added mistake not needed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,2 @@
 module ApplicationHelper
-  def nav_link(text, path)
-    class_name = 'nav-link'
-    class_name += ' active' if current_page?(path)
-
-    link_to text, path, class: class_name
-  end
 end
