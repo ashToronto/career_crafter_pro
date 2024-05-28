8648e2cc2fc119d10d76ca31f2de1be47c94c59d
Ashwin Khanna <ashwin.ashwinkhanna@gmail.com>
2024-05-28 17:39:55 -0400

    remove bugged social links from pie chart score calculations

--- app/services/resume_score_service.rb
+++ app/services/resume_score_service.rb
@@ -9,29 +9,16 @@ class ResumeScoreService
 
     # Weighted values
     weights = {
-      experiences: 15,
-      educations: 10,
-      skills: 2,
-      cover_letter: 20,
-      youtube_url: 5,
-      github_url: 5,
-      twitter_url: 5,
-      instagram_url: 5,
-      linkedin_url: 10,
-      facebook_url: 5
+      experiences: 30,
+      educations: 20,
+      skills: 20,
+      cover_letter: 30
     }
 
     score += weights[:experiences] if @resume.experiences.any?
     score += weights[:educations] if @resume.educations.any?
     score += weights[:skills] if @resume.skills.any?
     score += weights[:cover_letter] if @resume.cover_letter.present?
-    score += weights[:youtube_url] if @resume.social_link.youtube_url.present?
-    score += weights[:github_url] if @resume.social_link.github_url.present?
-
-    score += weights[:twitter_url] if @resume.social_link.twitter_url.present?
-    score += weights[:instagram_url] if @resume.social_link.instagram_url.present?
-    score += weights[:linkedin_url] if @resume.social_link.linkedin_url.present?
-    score += weights[:facebook_url] if @resume.social_link.facebook_url.present?
 
     [score, max_score].min
   end
