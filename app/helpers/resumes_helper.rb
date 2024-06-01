b4041262d1b27cb93bf7c429e564b090137920dc
Ashwin Khanna <33923739+ashToronto@users.noreply.github.com>
2024-06-01 02:17:41 -0400

    Change themes & add previewer with styling updates also (#48)
    * update dashboard page and set order list along with image of theme
    
    * fix deletion bug due to inline link to resume show overlap with resume delete icon
    
    * update colors primary
    
    * update color scheme to plum brand colors
    
    * update theme selector ui
    
    * update save button styles
    
    * update infographics and save btn on theme selector
    
    * landing page infographics
    
    * add a pie chart with a resume completion score service class, add style sheet ensure mobile responsive
    
    * remove bugged social links from pie chart score calculations
    
    * use resume helper instead of service object in model to calculate completion score, update eager loading in home controller
    
    * updates to dashboard for icons and chart render
    
    * remove back button useless in edit form
    
    * add footer to work experiences card with datetime and remote work
    
    * swap conditional for nav responsive better ui
    
    * update resume edit new placement of save btn
    
    * copy
    
    * re-do pdf layouts and use component architecture resue pdf and haml for previews, decouple resume sections from rendering pdf
    
    * contain styling within component partials

--- app/helpers/resumes_helper.rb
+++ app/helpers/resumes_helper.rb
@@ -1,2 +1,23 @@
 module ResumesHelper
+  def calculate_completion_score(resume)
+    score = 0
+    max_score = 100
+
+    # Define the weights for each component
+    weights = {
+      experiences: 30,
+      educations: 20,
+      skills: 20,
+      cover_letter: 30
+    }
+
+    # Add to score based on existence of resume parts
+    score += weights[:experiences] if resume.experiences.any?
+    score += weights[:educations] if resume.educations.any?
+    score += weights[:skills] if resume.skills.any?
+    score += weights[:cover_letter] if resume.cover_letter.present?
+
+    # Ensure the score does not exceed the maximum allowed
+    [score, max_score].min
+  end
 end
