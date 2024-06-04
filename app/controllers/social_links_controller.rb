e534bc71900d6f84c7ccae07ca286240c914a38a
Ashwin Khanna <ashwin.ashwinkhanna@gmail.com>
2024-06-03 21:49:39 -0400

    specs testing for social links #50

--- app/controllers/social_links_controller.rb
+++ app/controllers/social_links_controller.rb
@@ -1,42 +1,35 @@
 class SocialLinksController < ApplicationController
   before_action :authenticate_user!
-
   before_action :set_resume
   before_action :set_social_link, only: %i[edit update destroy]
+  before_action :ensure_at_least_one_social_link, only: [:create]
 
   def new
-    if @resume.social_link.present?
-      redirect_to edit_resume_social_link_path(@resume)
-    else
-      @social_link = @resume.social_link || @resume.build_social_link
-    end
+    @social_link = @resume.social_link || @resume.build_social_link
   end
 
   def create
     @social_link = @resume.build_social_link(social_link_params)
-    if @social_link.save
-      redirect_to resume_path(@resume), notice: 'Social links were successfully added.'
-    else
-      puts @social_link.errors.full_messages.to_sentence
-      redirect_to new_resume_social_link_path(@resume),
-                  alert: 'Social links were not successfully updated please check url.'
-    end
+    @social_link.save!
+    redirect_to resume_path(@resume), notice: 'Social links were successfully added.'
+  rescue ActiveRecord::RecordInvalid
+    redirect_to new_resume_social_link_path(@resume), alert: 'Not all Urls saved please use appropriate format'
   end
 
   def destroy
-    @social_link.destroy
+    @social_link.destroy!
     redirect_to resume_path(@resume), notice: 'Links section was successfully removed.'
   end
 
-  def edit; end
+  def edit
+    # No changes needed here
+  end
 
   def update
-    if @social_link.update(social_link_params)
-      redirect_to resume_path(@resume), notice: 'Social links were successfully updated.'
-    else
-      puts @social_link.errors.full_messages.to_sentence
-      redirect_to edit_resume_social_link_path(@resume), alert: 'Social links were not successfully updated.'
-    end
+    @social_link.update!(social_link_params)
+    redirect_to resume_path(@resume), notice: 'Social links were successfully updated.'
+  rescue ActiveRecord::RecordInvalid
+    redirect_to edit_resume_social_link_path(@resume), alert: 'Not all Urls were saved please use appropriate format'
   end
 
   private
@@ -46,11 +39,18 @@ class SocialLinksController < ApplicationController
   end
 
   def set_social_link
-    @social_link = @resume.social_link
+    @social_link = @resume.social_link || @resume.build_social_link
   end
 
   def social_link_params
     params.require(:social_link).permit(:linkedin_url, :github_url, :twitter_url, :youtube_url, :facebook_url,
                                         :instagram_url, :personal_website_url)
+  end
+
+  def ensure_at_least_one_social_link
+    return unless social_link_params.values.all?(&:blank?)
+
+    flash[:alert] = 'At least one social link must be provided.'
+    redirect_to new_resume_social_link_path
   end
 end
---
+++ spec/controllers/social_links_controller_spec.rb
@@ -0,0 +1,84 @@
+require 'rails_helper'
+
+RSpec.describe SocialLinksController, type: :controller do
+  let(:user) { create(:user) }
+  let(:resume) { create(:resume, user: user) }
+
+  before do
+    sign_in user
+  end
+
+  describe 'GET #new' do
+    context 'when a social link exists' do
+      let!(:social_link) { create(:social_link, resume: resume) }
+
+      it 'assigns the existing social link to @social_link' do
+        get :new, params: { resume_id: resume.id }
+        expect(assigns(:social_link)).to eq(social_link)
+      end
+    end
+
+    context 'when no social link exists' do
+      it 'assigns a new social link to @social_link' do
+        get :new, params: { resume_id: resume.id }
+        expect(assigns(:social_link)).to be_a_new(SocialLink)
+      end
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      let(:valid_attributes) { attributes_for(:social_link) }
+
+      it 'creates a new social link and redirects' do
+        expect do
+          post :create, params: { resume_id: resume.id, social_link: valid_attributes }
+        end.to change(SocialLink, :count).by(1)
+        expect(response).to redirect_to(resume_path(resume))
+      end
+    end
+
+    context 'with invalid attributes' do
+      let(:invalid_attributes) { { linkedin_url: 'not_a_url' } }
+
+      it 'does not create a social link and redirects' do
+        expect do
+          post :create, params: { resume_id: resume.id, social_link: invalid_attributes }
+        end.not_to change(SocialLink, :count)
+        expect(response).to redirect_to(new_resume_social_link_path(resume))
+      end
+    end
+  end
+
+  describe 'PUT #update' do
+    let(:social_link) { create(:social_link, resume: resume) }
+
+    context 'with valid attributes' do
+      it 'updates the social link and redirects' do
+        put :update, params: { resume_id: resume.id, id: social_link.id, social_link: { github_url: 'https://www.github.com/new_example' } }
+        social_link.reload
+        expect(social_link.github_url).to eq('https://www.github.com/new_example')
+        expect(response).to redirect_to(resume_path(resume))
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not update the social link and redirects' do
+        put :update, params: { resume_id: resume.id, id: social_link.id, social_link: { linkedin_url: 'bad_url' } }
+        social_link.reload
+        expect(response).to redirect_to(edit_resume_social_link_path(resume))
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let!(:social_link) { create(:social_link, resume: resume) }
+
+    it 'destroys the requested social link and redirects' do
+      expect do
+        delete :destroy, params: { resume_id: resume.id, id: social_link.id }
+      end.to change(SocialLink, :count).by(-1)
+      expect(response).to redirect_to(resume_path(resume))
+    end
+  end
+end
