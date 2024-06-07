a4c3a12e3ed858903eb1ca248aa75dd8ec4dc190
Ashwin Khanna <ashwin.ashwinkhanna@gmail.com>
2024-06-07 13:40:24 -0400

    add feature flag for devise mailers

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable,
+         *(:confirmable if ENV['DEVISE_EMAIL_CONFIRMATION_FEATURE_FLAG'] == 'true')
 
   enum role: { candidate: 0, admin: 1 }
 
