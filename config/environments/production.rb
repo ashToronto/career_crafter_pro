58f6fd99e70764010ba76c826477166bcb276ca7
Ashwin Khanna <ashwin.ashwinkhanna@gmail.com>
2024-06-10 18:59:45 -0400

    #61 add mail send from subdomain mailer ses

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,7 @@ Rails.application.configure do
     enable_starttls_auto: true
   }
   config.action_mailer.default_url_options = { host: ENV['SES_VERIFIED_DOMAIN'], protocol: 'https' }
+  config.action_mailer.default_options = { from: 'info@career-crafter-pro.com' } # This should be a verified sender
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
