16047c235ac942de41d2991581bf21717de7722e
Ashwin Khanna <ashwin.ashwinkhanna@gmail.com>
2024-07-18 18:36:18 -0400

    #114 add loading spinner for iframe web resume show page

---
+++ app/javascript/controllers/spinner_controller.js
@@ -0,0 +1,29 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["spinner"]
+
+  connect() {
+    // console.log("SpinnerController connected")
+    if (this.hasSpinnerTarget) {
+      console.log("Spinner target found")
+      this.showSpinner()
+    } else {
+      console.error("Missing spinner target")
+    }
+  }
+
+  showSpinner() {
+    // console.log("Show spinner")
+    this.spinnerTarget.style.display = "block"
+    setTimeout(() => {
+      this.hideSpinner()
+    }, 5136) // 5 milliseconds delay
+  }
+
+  hideSpinner() {
+    // console.log("Hide spinner")
+    this.spinnerTarget.style.display = "none"
+    this.iframeTarget.style.visibility = "visible"
+  }
+}
