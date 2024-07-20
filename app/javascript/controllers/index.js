dd61eb2fe3e276df14414c4a8269a77cb4eee8df
Ashwin Khanna <33923739+ashToronto@users.noreply.github.com>
2024-07-20 09:19:04 -0400

    #118 update resume show page, add accordian view to replace card view (#120)
    * #118 update resume show page, add accordian view to replace card view
    
    * #114 add loading spinner for iframe web resume show page
    
    * pr fix address comment for consistancy in ordering of sections within accordian to match sidebar

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,6 +2,9 @@
 
 import { application } from "controllers/application"
 
+import SpinnerController from "./spinner_controller"
+application.register("spinner", SpinnerController)
+
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
