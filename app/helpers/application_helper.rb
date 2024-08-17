1aa40859d7a4f0490cbe60816877904c9376987a
Ashwin Khanna <ashwin.ashwinkhanna@gmail.com>
2024-08-17 12:58:45 -0400

    #146 added location to free default template work experience section

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
       educations: [Education.new(
         institution_name: 'University of Tech',
         location: 'Learning Lane, New York, USA',
-        degree: 1,  # Assuming '1' correlates to an enum or similar representation
+        degree: 1, # Assuming '1' correlates to an enum or similar representation
         start_date: Date.new(2010, 1, 1),
         end_date: Date.new(2014, 1, 1)
       )],
@@ -22,16 +22,19 @@ module ApplicationHelper
 
     # Creating and assigning a cover letter
     cover_letter = CoverLetter.new
-    cover_letter.content = "I am very excited to apply for the Software Developer position. I believe my skills in Ruby on Rails and JavaScript make me a strong candidate."
+    cover_letter.content = 'I am very excited to apply for the Software Developer position. I believe my skills in Ruby on Rails and JavaScript make me a strong candidate.'
     resume.cover_letter = cover_letter
 
     # Adding experiences
     resume.experiences.build(
       company_name: 'Tech Inc.',
       job_title: 'Senior Developer',
+      country: 'Canada',
+      city: 'Toronto',
+      province: 'Ontario',
       start_date: Date.new(2018, 1, 1),
       end_date: Date.today
-    ).content = "<ul><li>Developed scalable web applications using Ruby on Rails</li> <li>Lead a team of developers in agile project settings.</li> <li>Implemented RESTful APIs for mobile applications</li></ul>"
+    ).content = '<ul><li>Developed scalable web applications using Ruby on Rails</li> <li>Lead a team of developers in agile project settings.</li> <li>Implemented RESTful APIs for mobile applications</li></ul>'
 
     resume
   end
